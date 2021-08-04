3d87eef03cd4619c54eff283a9d854bcc2341f24
manabushimada <manabu.shimada@mac.com>
2021-08-04 12:59:29 +0100

    kick off

---
+++ bitcoin-color-gradient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bitcoin-color-gradient
+//
+//  Created by Manabu Shimada on 04/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
